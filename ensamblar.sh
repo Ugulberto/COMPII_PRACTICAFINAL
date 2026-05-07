6a15fb82339e3a828ad1307ce1161dc3c5f3ef1e
David Blanco <82779923+Ugulberto@users.noreply.github.com>
2026-05-07 20:34:50 +0200

    Update ensamblar.sh

--- ensamblar.sh
+++ ensamblar.sh
@@ -1,3 +1,5 @@
+# Los comandos "ensambla" y "limpia" son los utilizados durante las clases de prácticas de computadores II.
+# Se pueden instalar desde studium.
 ensambla morse libraries/stdio libraries/stringH libraries/charMorse tables/chars_table tables/morse_table submenu1/mostrar_tabla submenu2/caracter_a_caracter submenu2/palabra_a_palabra submenu2/linea_a_linea submenu3/caracter_a_caracter submenu3/palabra_a_palabra submenu3/linea_a_linea
 
 limpia > /dev/null 2>&1
